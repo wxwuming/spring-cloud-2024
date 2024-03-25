6144fc467e4defc24b4615786c1d5207af1324bb
wuming <1322253919@qq.com>
2024-03-25 16:58:37 +0800

    a

--- cloud-api-commons/src/main/java/com/wuming/cloud/apis/PayFeignApi.java
+++ cloud-api-commons/src/main/java/com/wuming/cloud/apis/PayFeignApi.java
@@ -53,4 +53,7 @@ public interface PayFeignApi {
 
     @GetMapping(value = "/pay/get/info")
     public String mylb();
+
+    @GetMapping(value = "/pay/circuit/{id}")
+    public String myCircuit(@PathVariable("id") Integer id);
 }
---
+++ cloud-provider-payment8001/src/main/java/com/wuming/cloud/controller/PayCircuitController.java
@@ -0,0 +1,24 @@
+package com.wuming.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: wuming
+ * @DateTime: 2024/3/25 15:40
+ **/
+@RestController
+public class PayCircuitController {
+    @GetMapping(value = "/pay/circuit/{id}")
+    public String myCircuit(@PathVariable("id") Integer id){
+        if(id == -4) throw new RuntimeException("----circuit id 不能负数");
+        if(id == 9999){
+            try { TimeUnit.SECONDS.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+        return "Hello, circuit! inputId:  "+id+" \t " + IdUtil.simpleUUID();
+    }
+}
